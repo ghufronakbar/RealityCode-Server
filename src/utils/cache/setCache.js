e1b392ba13f7695c38f085b0eed15dfc24a17ed0
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-11 06:28:41 +0700

    update cache

--- src/utils/cache/setCache.js
+++ src/utils/cache/setCache.js
@@ -1,28 +1,27 @@
 const cache = require('./index');
 
-const setCache = (duration, useDecoded = false) => (req, res, next) => {
-    let key = req.originalUrl;    
-    if(useDecoded && req.decoded ){
-        if (req.decoded.wargaId >= 0) {                    
-            key = `${key}@wargaId${req.decoded.wargaId}`;
-        }else if (req.decoded.pengurusDesaAnggotaId >= 0) {
-            key = `${key}@pengurusDesaAnggotaId${req.decoded.pengurusDesaAnggotaId}`;        
-        }    
-    }
-    const cachedResponse = cache.get(key);    
+const setCache = (duration) => (req, res, next) => {
+    const key = req.originalUrl;
+
+    const cachedResponse = cache.get(key);
     if (cachedResponse) {
-        console.log("⚡ Using cache: ", key);
-        const jsonCache = JSON.parse(cachedResponse)
+        console.log("⚡  Using cache: ", key);
+        const jsonCache = JSON.parse(cachedResponse);
         return res.send(jsonCache);
     } else {
-        console.log("⚡ Setting cache: ", key);
+        console.log("👨‍🔬  Setting cache: ", key);
         res.originalSend = res.send;
         res.send = (body) => {
+            if (res.statusCode >= 200 && res.statusCode < 400) {
+                console.log("⚒️  Caching response for: ", key);
+                cache.set(key, body, duration);
+            } else {
+                console.log("😵  Not caching response due to error status: ", res.statusCode);
+            }
             res.originalSend(body);
-            cache.set(key, body, duration);
-        }
+        };
         next();
     }
-}
+};
 
 module.exports = setCache;
