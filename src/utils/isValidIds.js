1c40c277803fb7e794687976116f2c706b2c326c
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-12 05:04:24 +0700

    bug fix rill cuy

--- src/utils/isValidIds.js
+++ src/utils/isValidIds.js
@@ -2,11 +2,23 @@ const isValidIds = (data) => {
     if (!Array.isArray(data)) {
         return false;
     }
+
     for (let item of data) {
-        if (typeof item !== 'object' || !item.hasOwnProperty('id') || typeof item.id !== 'number') {
+        if (typeof item !== 'object' || item === null) {
+            return false;
+        }
+
+        const keys = Object.keys(item);
+
+        if (keys.length !== 1 || keys[0] !== 'id') {
+            return false;
+        }
+
+        if (typeof item.id !== 'number') {
             return false;
         }
     }
+
     return true;
 }
 
