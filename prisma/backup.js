0ec8d802d7a6e8730ad652eb39737a71302410b0
ghufron akbar <alan.040219.gam@gmail.com>
2024-09-25 15:56:01 +0700

    add backup

--- prisma/backup.js
+++ prisma/backup.js
@@ -6,35 +6,31 @@ const prisma = new PrismaClient();
 
 async function backupDatabase() {
   try {
-    // Query semua data dari setiap model
+
     const admins = await prisma.admin.findMany();
     const links = await prisma.link.findMany();
     const messages = await prisma.message.findMany();
     const posts = await prisma.post.findMany({
-      include: { images: true }  // Include related images in the posts
+      include: { images: true, subsection: { include: { posts: true } } }
     });
 
-    // Gabungkan semua data ke dalam satu objek
     const backupData = {
       admins,
       links,
       messages,
-      posts,  // images already included in posts
+      posts,
     };
 
-    // Tentukan path dan nama file untuk menyimpan JSON backup
     const backupFilePath = path.join(__dirname, `backup_${new Date().toISOString().replace(/:/g, '-')}.json`);
 
-    // Simpan data sebagai JSON
     fs.writeFileSync(backupFilePath, JSON.stringify(backupData, null, 2));
 
     console.log(`Backup berhasil disimpan ke: ${backupFilePath}`);
   } catch (error) {
     console.error("Terjadi kesalahan saat melakukan backup:", error);
   } finally {
-    await prisma.$disconnect();  // Pastikan koneksi ke database ditutup
+    await prisma.$disconnect();
   }
 }
 
-// Jalankan fungsi backup
 backupDatabase();
